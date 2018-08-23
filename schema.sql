37179183e5d6110d13f868799fe13e26d5972c46
Steven Berk <steven.e.berk@gmail.com>
2018-08-23 14:35:40 -0400

    SQL practice

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table phonebook(
+    id integer NOT NULL,
+    name CHARACTER VARYING(255),
+    number character VARYING(255)
+);
+
---
+++ seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO phonebook
+    (id, name, number)
+    values (1, 'yes', '12');
+
+INSERT INTO phonebook
+    (id, name, number)
+    values (2, 'Hello', '10');
